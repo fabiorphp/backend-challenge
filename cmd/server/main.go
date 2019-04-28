ad8d8086325f7f5c03a1f534ff68b605fdd08d60
Fabio Ribeiro <fabiorphp@gmail.com>
2019-04-28 09:56:25 -0300

    Add http router
    Gorilla mux router was added to simplify http endpoints.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/fabiorphp/backend-challenge/pkg/handler"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
@@ -17,9 +18,10 @@ func main() {
 	log.Printf("%s service - %s", appName, version)
 	log.Printf("starting server on %s", address)
 
-	http.HandleFunc("/health", handler.Health)
+	router := mux.NewRouter()
+	router.HandleFunc("/health", handler.Health).Methods(http.MethodGet)
 
-	err := http.ListenAndServe(address, nil)
+	err := http.ListenAndServe(address, router)
 
 	if err != nil {
 		log.Fatal(err)
